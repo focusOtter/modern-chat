45fc60fcc40a2aa1b1bd792e2644ff85c36a327d
Michael Liendo <mtliendo@gmail.com>
2022-08-15 23:11:17 -0500

    add ability to create a room

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import { Button, Link, withAuthenticator } from '@aws-amplify/ui-react'
+import {
+	Button,
+	Flex,
+	Link,
+	Text,
+	TextField,
+	View,
+	withAuthenticator,
+} from '@aws-amplify/ui-react'
 import { API } from 'aws-amplify'
 import { useEffect, useState } from 'react'
 import { listRooms } from '../src/graphql/queries'
+import { createRoom } from '../src/graphql/mutations'
 import NextLink from 'next/link'
 import config from '../src/aws-exports'
 import { Amplify } from 'aws-amplify'
 
 Amplify.configure({ ...config, ssr: true })
+
 function Home({ signOut, user }) {
 	const [rooms, setRooms] = useState([])
+	const [roomName, setRoomName] = useState('')
+
 	useEffect(() => {
 		API.graphql({
 			query: listRooms,
@@ -16,9 +28,38 @@ function Home({ signOut, user }) {
 			setRooms(data.listRooms.items)
 		})
 	}, [])
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		const { data } = await API.graphql({
+			query: createRoom,
+			variables: {
+				input: {
+					name: roomName,
+				},
+			},
+		})
+
+		setRooms([...rooms, data.createRoom])
+	}
+
 	return (
-		<>
-			Hey, {user.username}! Select a room to get started:
+		<View>
+			<Flex justifyContent={'end'}>
+				<Button onClick={signOut}>Sign Out</Button>
+			</Flex>
+			<Text marginBlockEnd={'relative.large'}>
+				Hey, {user.username}! Select a room to chat in or create your own public
+				room.
+			</Text>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					width="300px"
+					label="New Room Name"
+					placeholder="product-love"
+					onChange={(e) => setRoomName(e.target.value)}
+				/>
+			</form>
 			<ul>
 				{rooms.map((room) => (
 					<li key={room.id}>
@@ -28,8 +69,7 @@ function Home({ signOut, user }) {
 					</li>
 				))}
 			</ul>
-			<Button onClick={signOut}>Sign Out</Button>
-		</>
+		</View>
 	)
 }
 
